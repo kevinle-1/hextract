2233e992aa13f0e0cef15738e27c9b6985079f99
Kevin Le <github@kevinle.com.au>
2022-12-27 19:47:27 +0800

    custom bodypix config

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,24 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
-import sampleImg from '../sample/firefox_9HJSePiW27.png';
+import sampleImg from '../sample/emma-gy.jpg';
 
 import * as bodySegmentation from '@tensorflow-models/body-segmentation';
 import '@tensorflow/tfjs-backend-webgl';
 
 const Home: NextPage = () => {
-  const model = bodySegmentation.SupportedModels.MediaPipeSelfieSegmentation; // or 'BodyPix'
-  // const model = bodySegmentation.SupportedModels.BodyPix; // or 'BodyPix'
+  //const model = bodySegmentation.SupportedModels.MediaPipeSelfieSegmentation; // or 'BodyPix'
+   const model = bodySegmentation.SupportedModels.BodyPix; // or 'BodyPix'
 
   const bodyPixConfig = {
+    architecture: 'ResNet50',
+    multiplier: 1,
     multiSegmentation: false,
     segmentBodyParts: true,
-    refineSteps: 500
+    modelType: "general"
   }
 
-  bodySegmentation.createSegmenter(model, { runtime: 'tfjs' }).then(async (segmenter) => {
+  bodySegmentation.createSegmenter(model, bodyPixConfig).then(async (segmenter) => {
     const image = document.getElementById("image") as HTMLImageElement;
     const segmentation = await segmenter.segmentPeople(image, bodyPixConfig);
 
@@ -27,31 +29,14 @@ const Home: NextPage = () => {
     // the person and blurred background is blurred by 3 pixels.
     // await bodySegmentation.drawBokehEffect(canvas, image, segmentation);
 
-    const foregroundColor = {r: 255, g: 255, b: 255, a: 255};
-    const backgroundColor = {r: 0, g: 0, b: 0, a: 255};
+    const foregroundColor = {r: 255, g: 255, b: 255, a: 0};
+    const backgroundColor = {r: 255, g: 255, b: 255, a: 255};
     const backgroundDarkeningMask = await bodySegmentation.toBinaryMask(
         segmentation, foregroundColor, backgroundColor);
 
     const opacity = 1;
     const maskBlurAmount = 0; // range [0,20], default: 0
     const flipHorizontal = false;
-
-    // const canvas2 = document.getElementById('canvas2') as HTMLCanvasElement;
-    // const ctx = canvas2.getContext('2d')!;
-    // const { data: imgData } = ctx.getImageData(0, 0, canvas.width, canvas.height)
-
-    // const newImg = ctx.createImageData(image.width, image.height);
-    // const newImgData = newImg.data;
-
-    // segmentation.forEach(async (segment, i) => {
-    //     const img = await segment.mask.toImageData();
-
-    //     newImgData[i * 4] = img.data[i * 4]
-    //     newImgData[i * 4 + 1] = img.data[i * 4 + 1]
-    //     newImgData[i * 4 + 2] = img.data[i * 4 + 2]
-    //     newImgData[i * 4 + 3] = img.data[i * 4 + 3]
-    // })
-    // ctx.putImageData(newImg, 0, 0);
 
     await bodySegmentation.drawMask(
       canvas, image, backgroundDarkeningMask, opacity, maskBlurAmount, flipHorizontal);
@@ -61,9 +46,7 @@ const Home: NextPage = () => {
     canvas2.height = image.height;
 
     const ctx = canvas2.getContext('2d')!;
-
     const newImgData = await segmentation[0].mask.toImageData();
-
     ctx.putImageData(newImgData, 0, 0);
   });
 
