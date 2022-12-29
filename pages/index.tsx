90811e3c2eb6a5a413e3d186edde180ad200f503
Kevin Le <github@kevinle.com.au>
2022-12-29 23:34:21 +0800

    add palette link

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,14 @@ import styles from '../styles/Home.module.scss'
 
 import * as bodySegmentation from '@tensorflow-models/body-segmentation';
 import '@tensorflow/tfjs-backend-webgl';
+import Head from 'next/head';
+import Script from 'next/script';
 
 const r=0, g=1, b=2,a=3;
 
 const Home: NextPage = () => {
   const [processing, setProcessing] = useState<boolean>(false);
+  const [quantizedHexArray, setQuantizedHexArray] = useState<string[]>([]);
 
   const [exec, setExec] = useState<number | null>(null);
   const [imageLoaded, setImageLoaded] = useState<boolean>(false);
@@ -68,22 +71,37 @@ const Home: NextPage = () => {
         imageData = ctx.getImageData(0,0, canvas.width, canvas.height);
 
         const pixel = imageData.data;
+        let rgbArray = []
 
         for (var p = 0; p<pixel.length; p+=4)
         {
-          if (
+          if ( // White pixel
               pixel[p+r] == 255 &&
               pixel[p+g] == 255 &&
               pixel[p+b] == 255)
           {
-            pixel[p+a] = 0;
+            pixel[p+a] = 0; // Make transparent
           }
           else {
-            // TODO: Build [[R,G,B], ..., n] array for colour quantization
+            // Build [[R,G,B], ..., n] array for colour quantization
+            rgbArray.push([pixel[p+r], pixel[p+g], pixel[p+b]]);
           }
         }
 
         ctx.putImageData(imageData,0,0);
+
+        console.log("Begin quantization");
+        const quantize = require('quantize');
+        const colorMap = quantize(rgbArray, 6);
+        const quantizedResult: number[][] = colorMap.palette();
+
+        let bufferQuantizedHexArray: string[] = []
+        quantizedResult.forEach((rgb) => {
+          bufferQuantizedHexArray.push(rgbToHex(rgb[0], rgb[1], rgb[2]));
+        })
+
+        setQuantizedHexArray(bufferQuantizedHexArray);
+        console.log(bufferQuantizedHexArray);
 
         setProcessing(false);
         setImageLoaded(false);
@@ -116,19 +134,40 @@ const Home: NextPage = () => {
 
   return (
     <div className={styles.container}><br/>
+      {/* <Head>
+        <script src="https://coolors.co/palette-widget/widget.js"></script>
+      </Head> */}
+      <h2>Hextract (Proof of Concept)</h2>
+      <p>A colour palette generator with subject extraction.</p>
+      <p>Note: this will download ~100mb of Tensorflow models on first run - only use on uncapped data plans</p>
       <input type="file" onChange={e => loadImage(e, null)}/><br/><br/>
       <div>
-        {/* Note: CORS issues */}
+        {/* Note: CORS issues - Consider proxying URLs https://github.com/Rob--W/cors-anywhere*/}
         <input type="text" onChange={e => setUrl(e.target.value)} placeholder="Image URL (CORS ISSUES)"></input>
         <button onClick={e => loadImage(null, url)}>Load Image</button><br/><br/>
       </div>
       {/* <img id="image" className={styles.image} src={imageLoaded ? "" : url} crossOrigin="anonymous"></img> */}
-      <canvas id="canvas" className={styles.canvas}></canvas>
+      <canvas id="canvas" className={styles.canvas}></canvas><br/>
       {/* <canvas id="canvas2"></canvas> */}
+
+      {quantizedHexArray.length != 0 ? <a href={"https://coolors.co/" + quantizedHexArray.join("-")} target="_blank" rel="noreferrer">Palette</a> : null}
+
+      {/* {quantizedHexArray.length != 0 ? <div className={styles.palette}>
+        <Script data-id="09199291287329397">new CoolorsPaletteWidget("09199291287329397", {quantizedHexArray})</Script>
+      </div> : null} */}
       {processing ? <h4>Processing image...</h4> : null}
-      {exec != null ? <h4>Took {exec}ms to process</h4> : null}
+      {exec != null && !processing ? <h4>Took {exec}ms to process</h4> : null}
     </div>
   )
+}
+
+function componentToHex(c:number) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r:number, g:number, b:number): string {
+  return componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
 export default Home
